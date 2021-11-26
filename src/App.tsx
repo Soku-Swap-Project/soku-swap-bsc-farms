a1149bd843dade667d78ccd1889aaac888eb93d3
GobilINC <incredibledev.sec@gmail.com>
2021-11-26 13:23:30 -0500

    adding the coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import history from './routerHistory'
 import AccountModal from './components/AccountModal'
 import ClaimSokuModal from './components/ClaimSokuModal'
 import SlideOutMenu from './components/SlideOutMenu/SlideOutMenu'
+import ComingSoon from './views/ComingSoon'
 
 import './MobileFooter.css'
 
@@ -84,6 +85,9 @@ const App: React.FC = () => {
           </Route>
           <Route path="/bsc/staking/">
             <Pools />
+          </Route>
+          <Route path="/bsc/bridge/">
+            <ComingSoon />
           </Route>
           <Route component={NotFound} />
         </Switch>
