d20c44f4a3e9031d8418843f7fe7af1240eefdad
Demari C <demaricameron@outlook.com>
2022-02-22 16:46:28 -0500

    feat: statically add node urls

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -2,17 +2,19 @@ import random from 'lodash/random'
 
 // Array of available nodes to connect to
 export const nodes = [
-  process.env.BSC_PUBLIC_NODE_1,
-  process.env.BSC_PUBLIC_NODE_2,
-  process.env.BSC_PUBLIC_NODE_3,
-  process.env.BSC_PUBLIC_NODE_4,
-  process.env.BSC_PUBLIC_NODE_5,
-  process.env.BSC_PUBLIC_NODE_6,
-  process.env.BSC_PUBLIC_NODE_7,
-  process.env.BSC_PUBLIC_NODE_8,
-  process.env.BSC_PUBLIC_NODE_9,
-  process.env.BSC_PUBLIC_NODE_10,
+  'https://bsc-dataseed2.defibit.io/',
+  'https://bsc-dataseed3.defibit.io/',
+  'https://bsc-dataseed4.defibit.io/',
+  'https://bsc-dataseed2.ninicoin.io/',
+  'https://bsc-dataseed3.ninicoin.io/',
+  'https://bsc-dataseed4.ninicoin.io/',
+  'https://bsc-dataseed1.binance.org/',
+  'https://bsc-dataseed2.binance.org/',
+  'https://bsc-dataseed3.binance.org/',
+  'https://bsc-dataseed4.binance.org/',
 ]
+
+console.log(process.env, 'dotenv')
 
 const getNodeUrl = () => {
   const randomIndex = random(0, nodes.length - 1)
