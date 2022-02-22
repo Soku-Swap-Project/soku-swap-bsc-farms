ac654f1417c5495ec9b4681daa7c52696fd6a5c6
Demari C <demaricameron@outlook.com>
2022-02-22 16:24:43 -0500

    feat: update nodes

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,7 +1,18 @@
 import random from 'lodash/random'
 
 // Array of available nodes to connect to
-export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
+export const nodes = [
+  process.env.BSC_PUBLIC_NODE_1,
+  process.env.BSC_PUBLIC_NODE_2,
+  process.env.BSC_PUBLIC_NODE_3,
+  process.env.BSC_PUBLIC_NODE_4,
+  process.env.BSC_PUBLIC_NODE_5,
+  process.env.BSC_PUBLIC_NODE_6,
+  process.env.BSC_PUBLIC_NODE_7,
+  process.env.BSC_PUBLIC_NODE_8,
+  process.env.BSC_PUBLIC_NODE_9,
+  process.env.BSC_PUBLIC_NODE_10,
+]
 
 const getNodeUrl = () => {
   const randomIndex = random(0, nodes.length - 1)
