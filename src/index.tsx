b4d6a08b55db85ccdca26a8c2f76f0f420f0cd03
Demari C <demaricameron@outlook.com>
2022-02-23 14:32:08 -0500

    remove moralis provider

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,7 @@ import Providers from './Providers'
 ReactDOM.render(
   <React.StrictMode>
     <Providers>
-      <MoralisProvider
-        appId="dyZCUehXXgCqWeYktEJOCiJDrgkt4XSxGbt1BHC2"
-        serverUrl="https://dg42kg9qv9zz.usemoralis.com:2053/server"
-      >
-        <App />
-      </MoralisProvider>
+      <App />
     </Providers>
   </React.StrictMode>,
   document.getElementById('root'),
