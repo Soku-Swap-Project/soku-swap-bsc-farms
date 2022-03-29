72d49420fc9fed6fe266b3a634e0d6c4e0d071ea
Demari C <demaricameron@outlook.com>
2022-03-29 13:10:50 -0400

    feat: farm alert

--- src/components/Alerts/index.jsx
+++ src/components/Alerts/index.jsx
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
 
-function AlertDismissible() {
+function AlertDismissible(props) {
   const [show, setShow] = useState(true)
 
   if (show) {
     return (
-      <Alert style={{ background: '#FFF', marginTop: '-10px' }} onClose={() => setShow(false)} dismissible>
-        <Alert.Heading style={{ fontSize: '16px', fontWeight: 'bold' }}>Attention!</Alert.Heading>
-        <p>We opened all of the Vaults! You can find your staked tokens and rewards in the Finished tab.</p>
-      </Alert>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Alert
+          style={{ background: '#FFF', marginTop: '-10px', maxWidth: '500px' }}
+          onClose={() => setShow(false)}
+          dismissible
+        >
+          <Alert.Heading style={{ fontSize: '16px', fontWeight: 'bold' }}>Attention!</Alert.Heading>
+          <p>{props.children}</p>
+        </Alert>
+      </div>
     )
   }
   return null
