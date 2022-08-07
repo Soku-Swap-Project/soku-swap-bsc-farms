aebfe97804f54bb40c5c50ae98089710cc1ebb8c
ChefMari <demaricameron@outlook.com>
2022-08-07 15:41:40 -0400

    only allow withdraws

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,15 +1,13 @@
 import random from 'lodash/random'
 
 // Array of available nodes to connect to
-export const nodes = [
-  'https://bsc-dataseed4.ninicoin.io/'
-]
+export const nodes = ['https://speedy-nodes-nyc.moralis.io/a80a9a59f7e3ae9405c48919/bsc/mainnet']
 
 // console.log(process.env, 'dotenv')
 
 const getNodeUrl = () => {
   const randomIndex = random(0, nodes.length - 1)
-  return 'https://bsc-dataseed4.ninicoin.io/'
+  return 'https://speedy-nodes-nyc.moralis.io/a80a9a59f7e3ae9405c48919/bsc/mainnet'
 }
 
 export default getNodeUrl
