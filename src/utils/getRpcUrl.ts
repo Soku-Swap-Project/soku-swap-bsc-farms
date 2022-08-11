d549f322383d93eb2fd4fb716e688424436b2e15
ChefMari <demaricameron@outlook.com>
2022-08-11 08:53:43 -0400

    feat: update nodes

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,13 +1,13 @@
 import random from 'lodash/random'
 
 // Array of available nodes to connect to
-export const nodes = ['https://bsc‑dataseed2.ninicoin.io']
+export const nodes = ['https://bsc-dataseed.binance.org/']
 
 // console.log(process.env, 'dotenv')
 
 const getNodeUrl = () => {
   const randomIndex = random(0, nodes.length - 1)
-  return 'https://bsc‑dataseed2.ninicoin.io'
+  return 'https://bsc-dataseed.binance.org/'
 }
 
 export default getNodeUrl
